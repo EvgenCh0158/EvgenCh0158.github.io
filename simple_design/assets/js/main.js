009b7cfd4b737348fbee326107e2b49aed637d28
EvgenCh0158 <59840236+EvgenCh0158@users.noreply.github.com>
2020-01-30 19:55:59 +0200

    Update main.js

--- simple_design/assets/js/main.js
+++ simple_design/assets/js/main.js
@@ -1,40 +1,42 @@
-$('.people__img1').mouseenter(function(){
-  $('.profession__position').html('Jon Doe / CEO of LoremIpsum').animate({
-	      opacity: 1, // прозрачность элемента
-	      fontSize: "14px", // размер шрифта элемента  
-    });
-});
-	$('.people__img1').mouseleave(function(){   
-        $('.profession__position').animate({  
-	      opacity: 0, // прозрачность элемента
-	      fontSize: "0", // размер шрифта элемента 
-    });
-});
+$(function() {
+	$('.people__img1').mouseenter(function(){
+	  $('.profession__position').html('Jon Doe / CEO of LoremIpsum').animate({
+		      opacity: 1, // прозрачность элемента
+		      fontSize: "14px", // размер шрифта элемента  
+	    });
+	});
+		$('.people__img1').mouseleave(function(){   
+		$('.profession__position').animate({  
+		      opacity: 0, // прозрачность элемента
+		      fontSize: "0", // размер шрифта элемента 
+	    });
+	});
 
-	
-$('.people__img2').mouseenter(function(){
-    $('.profession__position').html('Nick Brown / Designer').animate({  
-	      opacity: 1, // прозрачность элемента
-	      fontSize: "14px", // размер шрифта элемента     
-    });
-}); 
-    $('.people__img2').mouseleave(function(){   
-        $('.profession__position').animate({  
-	      opacity: 0, // прозрачность элемента
-	      fontSize: "0", // размер шрифта элемента 
-    });
-});
-	
-	
-$('.people__img3').mouseenter(function(){
-    $('.profession__position').html('Bob Swansey / Web Developer').animate({  
-	      opacity: 1, // прозрачность элемента
-	      fontSize: "14px", // размер шрифта элемента     
-    });
-}); 
-    $('.people__img3').mouseleave(function(){   
-        $('.profession__position').animate({  
-	      opacity: 0, // прозрачность элемента
-	      fontSize: "0", // размер шрифта элемента 
-    });   	
+
+	$('.people__img2').mouseenter(function(){
+	    $('.profession__position').html('Nick Brown / Designer').animate({  
+		      opacity: 1, // прозрачность элемента
+		      fontSize: "14px", // размер шрифта элемента     
+	    });
+	}); 
+	    $('.people__img2').mouseleave(function(){   
+		$('.profession__position').animate({  
+		      opacity: 0, // прозрачность элемента
+		      fontSize: "0", // размер шрифта элемента 
+	    });
+	});
+
+
+	$('.people__img3').mouseenter(function(){
+	    $('.profession__position').html('Bob Swansey / Web Developer').animate({  
+		      opacity: 1, // прозрачность элемента
+		      fontSize: "14px", // размер шрифта элемента     
+	    });
+	}); 
+	    $('.people__img3').mouseleave(function(){   
+		$('.profession__position').animate({  
+		      opacity: 0, // прозрачность элемента
+		      fontSize: "0", // размер шрифта элемента 
+	    });   	
+	});
 });
