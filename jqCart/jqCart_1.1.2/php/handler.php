cb2c2ca4f118b5966021990314c02f0735cecaba
EvgenCh0158 <59840236+EvgenCh0158@users.noreply.github.com>
2020-02-18 08:23:40 +0200

    Update handler.php

--- jqCart/jqCart_1.1.2/php/handler.php
+++ jqCart/jqCart_1.1.2/php/handler.php
@@ -77,17 +77,17 @@ $headers[] = 'X-Mailer: PHP/'.phpversion();
 $send_ok = mail($to, $subject, $body, implode("\r\n", $headers));
 
 // Ответ на запрос
-/*
+
 $response = [
 	'errors' => !$send_ok,
 	'message' => $send_ok ? 'Заказ принят в обработку!' : 'Хьюстон! У нас проблемы!'
 ];
-*/
-// ! Для версий PHP < 5.4 использовать традиционный синтаксис инициализации массивов:
 
+// ! Для версий PHP < 5.4 использовать традиционный синтаксис инициализации массивов:
+/*
 $response = array (
 	'errors' => !$send_ok,
 	'message' => $send_ok ? 'Заказ принят в обработку!' : 'Хьюстон! У нас проблемы!'
 );
-
+*/
 exit( json_encode($response) );
